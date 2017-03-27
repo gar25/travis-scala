b186b3f7e4045e69ef0eef95936ec7fa0c3bb08a
gar25 <grc266@nyu.edu>
2017-03-27 19:25:30 -0400

    commit

---
+++ src/main/scala-2.12/Hello_World.scala
@@ -0,0 +1,11 @@
+/**
+  * Created by Garrett on 3/27/2017.
+  */
+class Hello_World {
+  def main(args: Array[String]): Unit ={
+    println("Hello World!")
+  }
+  def return_hello():String ={
+    "Hello"
+  }
+}
---
+++ src/test/scala-2.12/TestSet1.scala
@@ -0,0 +1,11 @@
+/**
+  * Created by Garrett on 3/27/2017.
+  */
+import org.junit.Assert;
+import org.junit.Test;
+
+class TestSet1 {
+  @Test
+  var x = new Hello_World
+  assert(x.return_hello() == "Hello")
+}
